e08b8207ee55b807fed25d7fe7e4b0641d48b0a4
trevorpreston <trevorpreston4@gmail.com>
2018-12-09 10:57:25 +0300

    add solution

--- base.sql
+++ base.sql
@@ -1,1 +1,14 @@
 -- add your seed data here!!!! 
+DROP DATABASE IF EXISTS cheeses_db;
+CREATE DATABASE cheeses_db;
+\c cheeses_db
+CREATE TABLE cheeses(id serial primary key, name varchar, color varchar, origin varchar, stink_level integer);
+INSERT INTO cheeses (name, color, origin, stink_level) VALUES
+('Roquefort', 'yellow', 'French', 5),
+('Epoisses', 'orange', 'French', 9),
+('Charolais', 'white', 'French', 5),
+('Maroilles', 'white', 'French', 10),
+('Durrus', 'yellow', 'Irish', 2),
+('Hooligan', 'yellow', 'American', 3),
+('Teleme', 'white', 'American', 2),
+('Stichelton', 'white', 'English', 4);
--- cheesy.sql
+++ cheesy.sql
@@ -1,17 +1,3 @@
-CREATE DATABASE cheeses_db;
-\c cheeses_db
-CREATE TABLE cheeses(id serial primary key, name varchar, color varchar, origin varchar, stink_level integer);
-INSERT INTO cheeses (name, color, origin, stink_level) VALUES
-('Roquefort', 'yellow', 'French', 5),
-('Epoisses', 'orange', 'French', 9),
-('Charolais', 'white', 'French', 5),
-('Maroilles', 'white', 'French', 10),
-('Durrus', 'yellow', 'Irish', 2),
-('Hooligan', 'yellow', 'American', 3),
-('Teleme', 'white', 'American', 2),
-('Stichelton', 'white', 'English', 4);
-
-
 -- **CHOOSING CHEESE**
 -- Find all the cheeses
 SELECT * FROM cheeses;
